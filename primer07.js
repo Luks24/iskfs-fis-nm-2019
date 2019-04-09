79bb6b2c00b9be134056ae1f06091eaac3b314e7
Luka Kidric <luka.kidric.24@gmail.com>
2019-04-09 12:22:15 +0000

    posodobil js pri primeru 7

--- primer07.js
+++ primer07.js
@@ -40,18 +40,38 @@ io.sockets.on("connection", function(socket) {
     
 }); // konec "sockets.on connection"
 
-board.digitalRead(2, function(value) {
-    if (value == 0) {
-        console.log("LED izklopljena");
-        board.digitalWrite(13, board.LOW);
-        pošljiVrednostPrekoVtičnika(0);
-    }
-    if (value == 1) {
-        console.log("LED vklopljena");
-        board.digitalWrite(13, board.HIGH);
-        pošljiVrednostPrekoVtičnika(1);
-    }
+var timeout = false;
+var zadnjaPoslana = null;
+var zadnjaVrednost = null;
+
+    board.digitalRead(2, function(value) { // digitalno branje se dogodi večkrat, ob spremembi stanja iz 0->1 ali 1->0
+        if (timeout !== false) { // če se je timeout spodaj pričel, (ob nestabilnem vhodu, npr. 0 1 0 1) ga biršemo
+    	   clearTimeout(timeout); // brišemo timeout dokler ni digitalni vhod stabilen, i.e. timeout = false
+    	   console.log("Timeout je postavljen na false");
+        }
+        timeout = setTimeout(function() { // ta del kode se bo izvedel po 50 ms; če se vmes dogodi sprememba stanja, ga gornja koda izbriše
+        // zgornja vrstica pomeni timeout = true
+            console.log("Timeout je postavljen na true");
+            timeout = false;
+            if (zadnjaVrednost != zadnjaPoslana) { // to send only on value change
+            	if (value == 0) {
+                    board.digitalWrite(13, board.LOW);
+                    console.log("Vrednost = 0, LED izklopljena");
+                    pošljiVrednostPrekoVtičnika(0);
+                }
+                else if (value == 1) {
+                    board.digitalWrite(13, board.HIGH);
+                    console.log("Vrednost = 1, LED prižgana");
+                    pošljiVrednostPrekoVtičnika(1);
+                }
     
-}); // konec "board.digitalRead"
+            }
+    
+            zadnjaPoslana = zadnjaVrednost;
+        }, 50); // izvedemo po 50ms
+                    
+        zadnjaVrednost = value; // ta vrednost se prebere iz nožice 2 večkrat na s
+        
+    }); // konec "board.digitalRead"
 
 }); // konec "board.on ready"
